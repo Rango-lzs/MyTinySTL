c3d89f3572540d4099443cd1c5bc16bff66a734c
lei-lzs <leizhengsheng2013@163.com>
2021-01-15 18:07:18 +0800

    for push

--- LzsSTL/include/vector.h
+++ LzsSTL/include/vector.h
@@ -11,6 +11,13 @@
 template<class T>
 class vector
 {
-	static_assert(!is_same(bool, T), "vector<bool> is not supported!");
+	static_assert(!std::is_same<bool, T>::value, "vector<bool> is not supported!");
+public:
+	//vector 的嵌套型别定义
+	typedef lzs::allocator<T> allocator_type;
+	typedef lzs::allocator<T> data_allocator;
+
+	typedef  typename allocator_type::value_type  value_type;
+
 };
 #endif
